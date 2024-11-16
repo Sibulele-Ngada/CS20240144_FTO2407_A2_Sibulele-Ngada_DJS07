b5f384367332ff07a644d7f6d51002ce9211a7ee
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-16 14:22:56 +0200

    Fix header image bug

--- DJS07/src/components/Header.jsx
+++ DJS07/src/components/Header.jsx
@@ -2,12 +2,12 @@ export default function Header() {
   return (
     <header className="header">
       <img
-        src="./assets/troll-face.png"
+        src="../assets/troll-face.png"
         className="header--image"
         alt="troll-face"
       />
       <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">React Course - Project 3</h4>
+      <h4 className="header--project">React Course - DJS07</h4>
     </header>
   );
 }
