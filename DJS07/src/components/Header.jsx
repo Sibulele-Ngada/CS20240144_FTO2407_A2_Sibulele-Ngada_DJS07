f54c9ed69e32c5f3c829616e6471ca8935de5eb6
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-19 13:06:41 +0200

    Icon image

--- DJS07/src/components/Header.jsx
+++ DJS07/src/components/Header.jsx
@@ -1,11 +1,9 @@
+import trollImg from "../assets/troll-face.png";
+
 export default function Header() {
   return (
     <header className="header">
-      <img
-        src="../assets/troll-face.png"
-        className="header--image"
-        alt="troll-face"
-      />
+      <img src={trollImg} className="header--image" alt="troll-face" />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">React Course - DJS07</h4>
     </header>
